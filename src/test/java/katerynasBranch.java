99cfb0c046042a471c7abf4ad0961c6bcaa63d61
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-15 22:03:44 +0200

    Kateryna commit help

--- src/test/java/katerynasBranch.java
+++ src/test/java/katerynasBranch.java
@@ -2,5 +2,6 @@ public class katerynasBranch {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Again");
+        System.out.println("Help");
     }
 }
